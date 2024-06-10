aa153bc8f20737fbb5867b7fda13d5c4df7c972b
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-06-10 16:45:36 +0200

    added values to the buttons and created a converting function

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -25,11 +25,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +56,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter() {
 
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("") }
+    var outputUnit by remember { mutableStateOf("") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember { mutableStateOf(0.001) }
+
+    fun theConverter() {
+
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
+        val results = (inputValueDouble * conversionFactor.value * 100.0).roundToInt() / 100.0
+        outputValue = results.toString()
+    }
+
     // here all the elements will be stacked below each other
     Column (modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -59,28 +79,35 @@ fun UnitConverter() {
         Spacer(modifier = Modifier.height(16.dp))
         // inside the onValueChange goes what should happen,
         // when the value of our OutlinedTextField changes.
-        OutlinedTextField(value = "", onValueChange = {
+        OutlinedTextField(value = inputValue, onValueChange = {
+            inputValue = it},
             // here goes what should happen, when the value of our
             // outline
-        })
+            label = { Text(text = "Enter a Value")}
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
         // here all the elements will be stacked next to each other
         Row {
+            // input box
             Box {
-                Button(onClick = { /*TODO*/ }) {
+                // input button
+                Button(onClick = {iExpanded = true}) {
                     Text(text = "Select")
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
-                    DropdownMenuItem(text = { Text(text = "Millimeter")},
+                DropdownMenu(expanded = iExpanded, onDismissRequest = {iExpanded = false}) {
+                    DropdownMenuItem(text = { Text("Millimeter")},
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "Millimeter"
+                            conversionFactor.value = 0.0001})
+                    DropdownMenuItem(text = { Text("Centimeter")},
                         onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Centimeter")},
+                    DropdownMenuItem(text = { Text("Meter")},
                         onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Meter")},
-                        onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Kilometer")},
+                    DropdownMenuItem(text = { Text("Kilometer")},
                         onClick = { /*TODO*/ }
                     )
                 }
@@ -89,13 +116,15 @@ fun UnitConverter() {
 
             Spacer(modifier = Modifier.width(16.dp))
 
+            // output box
             Box {
-                Button(onClick = { /*TODO*/ }) {
+                // output button
+                Button(onClick = {oExpanded = true}) {
                     Text(text = "Select")
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(expanded = oExpanded, onDismissRequest = {oExpanded = false}) {
                     DropdownMenuItem(text = { Text(text = "Millimeter")},
                         onClick = { /*TODO*/ })
                     DropdownMenuItem(text = { Text(text = "Centimeter")},
@@ -109,7 +138,7 @@ fun UnitConverter() {
             }
         }
 
-        //Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = "The results")
     }
 }
